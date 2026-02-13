e1c5f1ec04c029a192fd0b6d31c024a3ee1a7cf8
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-02-14 00:20:38 +0600

    Add admin index.blade.php file

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html> 
+  @include('admin.css')
+  <body>
+    @include('admin.header')
+     @include('admin.slidebar')
+   
+     @include('admin.body')
+
+        @include('admin.footer')
+  
