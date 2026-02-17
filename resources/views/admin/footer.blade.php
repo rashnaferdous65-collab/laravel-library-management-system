96dbbe5accd0413b041cccde351d45b1da12aa5b
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-02-18 00:54:40 +0600

    Added footer blade file

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,22 @@
+       <!-- Footer-->
+        <footer class="footer">
+          <div class="footer__block block no-margin-bottom">
+            <div class="container-fluid text-center">
+             
+               <p class="no-margin-bottom"> Project By 2025 &copy;Rashna Ferdous</p>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+    <!-- JavaScript files--> 
+    <script src="{{asset('admin_css/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('admin_css/vendor/popper.js/umd/popper.min.js')}}"> </script>
+    <script src=" {{asset('admin_css/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('admin_css/vendor/jquery.cookie/jquery.cookie.js')}}"> </script>
+    <script src="{{asset('admin_css/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src=" {{asset('admin_css/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('admin_css/js/charts-home.js')}}"></script>
+    <script src="{{asset('admin_css/js/front.js')}}"></script>
+  </body>
+</html>
