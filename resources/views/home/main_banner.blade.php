94b4b6a52e9d4c4158d17eee0f6cc1b5a4bdca22
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-02-23 00:09:30 +0600

    Refactored main_banner blade UI design

--- resources/views/home/main_banner.blade.php
+++ resources/views/home/main_banner.blade.php
@@ -1,43 +1,54 @@
+<!-- ===== Main Banner Section Start ===== -->
+<section class="main-banner py-5">
+  <div class="container">
+    <div class="row align-items-center">
 
-
-  <!-- ***** Main Banner Area Start ***** -->
-  <div class="main-banner">
-    <div class="container">
-      <div class="row">
-         @if(session()->has('message'))
-                 <div class="alert alert-success alert-dismissible fade show" role="alert">
-              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">x</button>
-
-              
-               {{session()->get('message')}}
-                </div>
-               @endif
-        <div class="col-lg-6 align-self-center">
-          <div class="header-text">
-            <h6>Book is Knowledge</h6>
-            <h2>Knowledge is Power</h2>
-            <p>Library is a really cool and professional design for your websites. This HTML CSS template is based on Bootstrap v5 and it is designed for related web portals. Liberty can be freely downloaded from github</p>
-            <div class="buttons">
-              <div class="border-button">
-                <a href="explore.html">Explore Top Books</a>
-              </div>
-              <div class="main-button">
-                <a href="" target="_blank">Watch Our Videos</a>
-              </div>
-            </div>
+      {{-- Success Message --}}
+      @if(session()->has('message'))
+        <div class="col-12">
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('message') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
         </div>
-        <div class="col-lg-5 offset-lg-1">
-          <div class="">
-            <div class="item">
-              <img src="assets/images/banner.png" alt="">
-            </div>
-            <div class="item">
-              <img src="assets/images/banner2.png" alt="">
-            </div>
+      @endif
+
+      <!-- Left Content -->
+      <div class="col-lg-6">
+        <div class="banner-content">
+          <span class="text-primary fw-bold">Book is Knowledge</span>
+          <h1 class="my-3">Knowledge is Power</h1>
+          <p class="text-muted">
+            Library is a modern and professional template built with Bootstrap 5.
+            Perfect for book portals and educational websites. Download and customize it easily from GitHub.
+          </p>
+
+          <div class="d-flex gap-3 mt-4">
+            <a href="{{ url('explore') }}" class="btn btn-outline-primary">
+              Explore Top Books
+            </a>
+
+            <a href="#" class="btn btn-primary" target="_blank">
+              Watch Our Videos
+            </a>
           </div>
         </div>
       </div>
+
+      <!-- Right Images -->
+      <div class="col-lg-5 offset-lg-1">
+        <div class="banner-images text-center">
+          <img src="{{ asset('assets/images/banner.png') }}" 
+               class="img-fluid mb-3" 
+               alt="Library Banner Image 1">
+
+          <img src="{{ asset('assets/images/banner2.png') }}" 
+               class="img-fluid" 
+               alt="Library Banner Image 2">
+        </div>
+      </div>
+
     </div>
   </div>
-  <!-- ***** Main Banner Area End ***** -->
+</section>
+<!-- ===== Main Banner Section End ===== -->
