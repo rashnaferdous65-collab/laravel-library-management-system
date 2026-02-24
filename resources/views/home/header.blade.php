d40ebcf152044ec190c0534a5661686991b468f7
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-02-25 01:14:46 +0600

    Refactored home header blade structure

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,56 +1,97 @@
- <!-- ***** Preloader Start ***** -->
-  <div id="js-preloader" class="js-preloader">
+<!-- ***** Preloader Start ***** -->
+<div id="js-preloader" class="js-preloader">
     <div class="preloader-inner">
-      <span class="dot"></span>
-      <div class="dots">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+        <span class="dot"></span>
+        <div class="dots">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
     </div>
-  </div>
-  <!-- ***** Preloader End ***** -->
+</div>
+<!-- ***** Preloader End ***** -->
 
-  <!-- ***** Header Area Start ***** -->
-  <header class="header-area header-sticky">
+
+<!-- ***** Header Area Start ***** -->
+<header class="header-area header-sticky">
     <div class="container">
         <div class="row">
             <div class="col-12">
+
                 <nav class="main-nav">
-                    <!-- ***** Logo Start ***** -->
-                    <a href="{{url('/')}}" class="logo">
-                        <img src="assets/images/logo.png" alt="">
+
+                    <!-- Logo -->
+                    <a href="{{ url('/') }}" class="logo">
+                        <img src="{{ asset('assets/images/logo.png') }}" alt="Logo">
                     </a>
-                    <!-- ***** Logo End ***** -->
-                    <!-- ***** Menu Start ***** -->
+
+                    <!-- Menu -->
                     <ul class="nav">
-                        <li><a href="{{route('home')}}"   class="{{ request()->routeIs('home') ? 'active' : '' }}">Home</a></li>
-                        <li><a href="{{url('explore')}}">Explore</a></li>
-                        <li><a href="{{url('create')}}">Create Yours</a></li>
-                    
+
+                        {{-- Home --}}
+                        <li>
+                            <a href="{{ route('home') }}"
+                               class="{{ request()->routeIs('home') ? 'active' : '' }}">
+                                Home
+                            </a>
+                        </li>
+
+                        {{-- Explore --}}
+                        <li>
+                            <a href="{{ url('explore') }}">
+                                Explore
+                            </a>
+                        </li>
+
+                        {{-- Create --}}
+                        <li>
+                            <a href="{{ url('create') }}">
+                                Create Yours
+                            </a>
+                        </li>
+
+                        {{-- Authenticated User Menu --}}
                         @auth
-                          <li><a href="{{url('book_history')}}">My History</a></li>
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <input type="submit" value="Logout" class="btn btn-transparent text-white">
-    </form>
-@endauth
+                            <li>
+                                <a href="{{ url('book_history') }}">
+                                    My History
+                                </a>
+                            </li>
 
-@guest
-    <li><a href="{{ route('login') }}">Log In</a></li>
-    <li><a href="{{ route('register') }}">Register</a></li>
-@endguest
+                            <li>
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button type="submit" 
+                                            class="btn btn-transparent text-white border-0 bg-transparent">
+                                        Logout
+                                    </button>
+                                </form>
+                            </li>
+                        @endauth
 
-                       
-                    </ul>   
-                    <a class='menu-trigger'>
+
+                        {{-- Guest User Menu --}}
+                        @guest
+                            <li>
+                                <a href="{{ route('login') }}">Log In</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('register') }}">Register</a>
+                            </li>
+                        @endguest
+
+                    </ul>
+
+                    <!-- Mobile Menu Trigger -->
+                    <a class="menu-trigger">
                         <span>Menu</span>
                     </a>
-                    <!-- ***** Menu End ***** -->
+
                 </nav>
-                 
+
             </div>
         </div>
     </div>
-  </header>
+</header>
+
   <!-- ***** Header Area End ***** -->
