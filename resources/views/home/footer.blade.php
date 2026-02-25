d4a1245bb26e2902235719def2ef5eb3cabe917a
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-02-26 01:41:56 +0600

    Update footer design with improved structure

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -1,29 +1,36 @@
-
-
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <p>Copyright © 2025 Project By Rashna Ferdous
-          &nbsp;&nbsp;
-          Designed by Rashna Ferdous</p>
+<!-- ====== Footer Section Start ====== -->
+<footer class="footer-section py-4">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-12">
+                <p class="mb-0">
+                    &copy; 2025 <strong>Project By Rashna Ferdous</strong>.
+                    <span class="mx-2">|</span>
+                    Designed & Developed by <strong>Rashna Ferdous</strong>
+                </p>
+            </div>
         </div>
-      </div>
     </div>
-  </footer>
+</footer>
+<!-- ====== Footer Section End ====== -->
 
 
-  <!-- Scripts -->
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- ====== JavaScript Files ====== -->
 
-  <script src="assets/js/isotope.min.js"></script>
-  <script src="assets/js/owl-carousel.js"></script>
+<!-- jQuery -->
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
 
-  <script src="assets/js/tabs.js"></script>
-  <script src="assets/js/popup.js"></script>
-  <script src="assets/js/custom.js"></script>
+<!-- Bootstrap JS -->
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
 
-  </body>
+<!-- Plugins -->
+<script src="{{ asset('assets/js/isotope.min.js') }}"></script>
+<script src="{{ asset('assets/js/owl-carousel.js') }}"></script>
+
+<!-- Custom Scripts -->
+<script src="{{ asset('assets/js/tabs.js') }}"></script>
+<script src="{{ asset('assets/js/popup.js') }}"></script>
+<script src="{{ asset('assets/js/custom.js') }}"></script>
+
+</body>
 </html>
