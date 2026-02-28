b32c31d770c12e1c428b6ad63f9b3f7a7161f5bc
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-03-01 00:36:22 +0600

    Updated category section with dynamic loop

--- resources/views/home/category.blade.php
+++ resources/views/home/category.blade.php
@@ -1,73 +1,42 @@
- <div class="categories-collections">
+<div class="categories-collections">
     <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="categories">
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="section-heading">
-                  <div class="line-dec"></div>
-                  <h2>Browse Through Book <em>Categories</em> Here.</h2>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="categories">
+                    <div class="row">
+
+                        <div class="col-lg-12">
+                            <div class="section-heading">
+                                <div class="line-dec"></div>
+                                <h2>Browse Through Book <em>Categories</em> Here.</h2>
+                            </div>
+                        </div>
+
+                        @php
+                            $categories = [
+                                ['name' => 'Motivational', 'icon' => 'icon-01.png'],
+                                ['name' => 'Money', 'icon' => 'icon-02.png'],
+                                ['name' => 'Psychological', 'icon' => 'icon-03.png'],
+                                ['name' => 'Story', 'icon' => 'icon-04.png'],
+                                ['name' => 'Fictional', 'icon' => 'icon-05.png'],
+                                ['name' => 'Romance', 'icon' => 'icon-06.png'],
+                            ];
+                        @endphp
+
+                        @foreach($categories as $category)
+                            <div class="col-lg-2 col-sm-6">
+                                <div class="item text-center">
+                                    <div class="icon">
+                                        <img src="{{ asset('assets/images/' . $category['icon']) }}" alt="{{ $category['name'] }}">
+                                    </div>
+                                    <h4>{{ $category['name'] }}</h4>
+                                </div>
+                            </div>
+                        @endforeach
+
+                    </div>
                 </div>
-              </div>
-              <div class="col-lg-2 col-sm-6">
-                <div class="item">
-                  <div class="icon">
-                    <img src="assets/images/icon-01.png" alt="">
-                  </div>
-                  <h4>Motivational</h4>
-                  
-                </div>
-              </div>
-              <div class="col-lg-2 col-sm-6">
-                <div class="item">
-                  <div class="icon">
-                    <img src="assets/images/icon-02.png" alt="">
-                  </div>
-                  <h4>Money</h4>
-                  
-                </div>
-              </div>
-              <div class="col-lg-2 col-sm-6">
-                <div class="item">
-                  <div class="icon">
-                    <img src="assets/images/icon-03.png" alt="">
-                  </div>
-                  <h4>Psychological</h4>
-                  
-                </div>
-              </div>
-              <div class="col-lg-2 col-sm-6">
-                <div class="item">
-                  <div class="icon">
-                    <img src="assets/images/icon-04.png" alt="">
-                  </div>
-                  <h4>Story</h4>
-                  
-                </div>
-              </div>
-              <div class="col-lg-2 col-sm-6">
-                <div class="item">
-                  <div class="icon">
-                    <img src="assets/images/icon-05.png" alt="">
-                  </div>
-                  <h4>Fictional</h4>
-                  
-                </div>
-              </div>
-              <div class="col-lg-2 col-sm-6">
-                <div class="item">
-                  <div class="icon">
-                    <img src="assets/images/icon-06.png" alt="">
-                  </div>
-                  <h4>Romance</h4>
-                  
-                </div>
-              </div>
             </div>
-          </div>
         </div>
-        
-      </div>
     </div>
-  </div>
+</div>
