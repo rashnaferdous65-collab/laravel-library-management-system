179b9dc80a1cecc3432a0aa594ebc36ba404d9e8
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-03-06 00:38:05 +0600

    Update forgot password blade UI

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,40 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+
+    <div class="text-sm text-gray-600 mb-4">
+        {{ __('If you forgot your password, enter your email address below. We will send you a link to reset your password.') }}
     </div>
 
-    <!-- Session Status -->
+    <!-- Session Message -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="mt-2">
+            <!-- Email Label -->
+            <x-input-label for="email" :value="__('Email Address')" />
+
+            <!-- Email Input -->
+            <x-text-input
+                id="email"
+                name="email"
+                type="email"
+                class="block w-full mt-1"
+                :value="old('email')"
+                required
+                autofocus
+            />
+
+            <!-- Error Message -->
+            <x-input-error :messages="$errors->get('email')" class="mt-2"/>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex justify-end items-center mt-5">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Send Reset Link') }}
             </x-primary-button>
         </div>
+
     </form>
+
 </x-guest-layout>
