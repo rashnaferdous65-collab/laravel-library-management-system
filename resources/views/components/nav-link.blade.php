1ad0cd2527071d04ff0d7f2701374075ed908f06
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-03-19 00:40:55 +0600

    Update nav-link component design

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,9 +1,13 @@
-@props(['active'])
+@props(['active' => false])
 
 @php
-$classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+$baseClasses = 'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none';
+
+$activeClasses = 'border-indigo-400 text-gray-900 focus:border-indigo-700';
+
+$inactiveClasses = 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300';
+
+$classes = $baseClasses . ' ' . ($active ? $activeClasses : $inactiveClasses);
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
