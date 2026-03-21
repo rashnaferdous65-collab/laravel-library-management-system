0a1dfa6f53d82082ec67b89a6361e46d1f08d4a8
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-03-22 00:08:39 +0600

    Update secondary button component design

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,14 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+@props(['type' => 'button'])
+
+@php
+    $baseClasses = 'inline-flex items-center px-4 py-2 border rounded-md font-semibold text-xs uppercase tracking-widest shadow-sm transition ease-in-out duration-150 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25';
+
+    $styleClasses = 'bg-white border-gray-300 text-gray-700 hover:bg-gray-50';
+@endphp
+
+<button {{ $attributes->merge([
+    'type' => $type,
+    'class' => $baseClasses . ' ' . $styleClasses
+]) }}>
     {{ $slot }}
 </button>
