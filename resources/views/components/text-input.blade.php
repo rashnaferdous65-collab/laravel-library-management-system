233bad54f7017b62e1eb78b4eaacbf5f57911a87
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-03-23 01:03:35 +0600

    Update text input component design

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,10 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+@php
+    $isDisabled = $disabled ? 'disabled' : '';
+@endphp
+
+<input {{ $isDisabled }}
+    {{ $attributes->merge([
+        'class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm'
+    ]) }}>
