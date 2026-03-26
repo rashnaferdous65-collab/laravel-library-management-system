4c2b86c9866b9163ce85bc88bab1579a42734c9a
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-03-27 00:13:09 +0600

    Refactor profile edit blade UI structure

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,40 @@
 <x-app-layout>
+    {{-- Header Section --}}
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold text-gray-800 leading-tight">
             {{ __('Profile') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+```
+{{-- Main Content --}}
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
+        {{-- Profile Info Update --}}
+        <section class="bg-white shadow sm:rounded-lg p-6">
+            <div class="max-w-xl mx-auto">
+                @include('profile.partials.update-profile-information-form')
             </div>
+        </section>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
+        {{-- Password Update --}}
+        <section class="bg-white shadow sm:rounded-lg p-6">
+            <div class="max-w-xl mx-auto">
+                @include('profile.partials.update-password-form')
             </div>
-        </div>
+        </section>
+
+        {{-- Delete Account --}}
+        <section class="bg-white shadow sm:rounded-lg p-6">
+            <div class="max-w-xl mx-auto">
+                @include('profile.partials.delete-user-form')
+            </div>
+        </section>
+
     </div>
+</div>
+```
+
 </x-app-layout>
+
